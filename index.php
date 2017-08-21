904d5ce2e3713b1a413a10beac56133d6a3e03ce
Airat <airat.mufazalov@live.ru>
2017-08-21 19:06:06 +0300

    nazvanie

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form.php">
+    <p>Представьтесь</p>
+    <input type="text" name="name">
+    <p>Укажите ваш номер</p>
+   <input type="text" name="phone">
+    <button type="submit">отправить</button>
+</form>
+
+</body>
+</html>
