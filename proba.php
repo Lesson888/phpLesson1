d715206398b8b603406d593b389fa96fcb2cb368
Airat <airat.mufazalov@live.ru>
2017-08-24 19:28:55 +0300

    ny

---
+++ proba.php
@@ -0,0 +1,32 @@
+<?php
+
+$test="Hello, world!";
+echo $test;
+
+
+$a1=5;
+$a2=0.5;
+
+echo $a1+$a2;
+
+$str = "Hello, world $test! <br />";
+
+$test = 'Test string';
+$str2 = "Hello, morning! $test";
+echo $str;
+
+$intl=10;
+$floatl=0.5;
+
+echo "$intl*$floatl";
+
+$bool1=true;
+$bool2=false;
+
+$a;
+
+
+
+$name="Anton";
+$names=array("Anton", "Anna", "Natasha");
+var_dump($name, $names);
