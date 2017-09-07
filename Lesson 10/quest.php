b4a9d400a9ac5493acfa2352ab585fd7937df51e
Airat <airat.mufazalov@live.ru>
2017-09-07 19:53:35 +0300

    QUESTY

---
+++ Lesson 10/quest.php
@@ -0,0 +1,90 @@
+<?php
+
+
+$question = '';
+$answers = [];
+$result = '';
+
+
+$steps = [
+    [
+        'id' => 1,
+        'question' => 'Влево или вправо',
+        'answers' => [
+            [
+                'text' => 'Влево',
+                'function' => 'next',
+                'next_step' => 2,
+            ],
+            [
+                'text' => 'Вправо',
+                'function' => 'endGame',
+            ],
+        ],
+    ],
+    [
+        'id' => 2,
+        'question' => 'Прыгать или бежать?',
+        'answers' => [
+            [
+                'text' => 'Прыгать',
+                'function' => 'next',
+                'next_step' => 3,
+            ],
+            [
+                'text' => 'Бежать',
+                'function' => 'endGame',
+            ]
+        ],
+    ],
+    [
+        'id' => 3,
+        'question' => 'Вперед или назад?',
+        'answers' => [
+            [
+                'text' => 'Вперед',
+                'function' => 'endGame',
+            ],
+            [
+                'text' => 'Назад',
+                'function' => 'win',
+            ]
+        ],
+    ],
+];
+
+function findNextStep($id, $steps) {
+    $step = null;
+    foreach ($steps as $_step) {
+        if ($_step['id'] == $id) {
+            $step = $_step;
+        }
+    }
+    return $step;
+}
+
+function generateQuestion ($step) {
+    return $step['question'];
+}
+
+function generateAnswers ($step) {
+    return $step['answers'];
+}
+
+if (isset($_POST['submit'])) {
+    $answer = json_decode($_POST['answer'], true);
+    if ($answer['function'] === 'next') {
+        $step = findNextStep($answer['next_step'], $steps);
+        $question = generateQuestion($step);
+        $answers = generateAnswers($step);
+    } else if ($answer['function'] === 'endGame') {
+        $result = 'Вы проиграли';
+    } else if ($answer['function'] === 'win') {
+        $result = 'Вы победили';
+    }
+} else {
+    $step = findNextStep(1, $steps);
+    $question = generateQuestion($step);
+    $answers = generateAnswers($step);
+
+}
