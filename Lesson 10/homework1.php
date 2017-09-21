385896d34c929f7e4cf61e572bbb4b61f4080a23
Natalia Zakharova <natalia_zakharova-93@mail.ru>
2017-09-21 16:30:08 +0300

    База данных

--- Lesson 10/homework1.php
+++ Lesson 10/homework1.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Обработка форм. Урок №19.</title>
+    <title>Обработка форм. Урок №19. Домашняя работа</title>
 </head>
 <body>
 
